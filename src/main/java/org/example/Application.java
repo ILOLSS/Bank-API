23da4932a381b7d55ea396f2a942bd5785d44bae
ILOLSS <69246965+ILOLSS@users.noreply.github.com>
2021-02-28 10:16:19 +0300

    deploy heroku

--- src/main/java/org/example/Application.java
+++ src/main/java/org/example/Application.java
@@ -1,15 +1,7 @@
 package org.example;
 
-//import org.codehaus.jackson.map.ObjectMapper;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//import org.springframework.http.converter.json.MappingJacksonHttpMessageConverter;
-import org.springframework.web.client.RestTemplate;
-import org.example.model.ValCurs;
-
-import java.io.IOException;
-import java.io.StringReader;
 
 @SpringBootApplication
 public class Application {
